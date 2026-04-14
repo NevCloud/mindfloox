6dbd6ae0ba639b1e8eede3b36c24a5601846aeda
NevCloud <fahadarifin15@gmail.com>
2026-04-14 22:42:37 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,32 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('index');
-});
+use App\Http\Controllers\HomeController;
+Route::get('/', [HomeController::class, 'index'])->name('index');
 
 Route::get('/login', function () {
     return view('login');
-});
+})->name('login');
 
+// Halaman About
 Route::get('/about', function () {
     return view('about');
-});
+})->name('about');
 
+// Halaman Course
+Route::get('/course', function () {
+    return view('courses');
+})->name('courses');
+
+// Halaman Instructors
+use App\Http\Controllers\instructorsController;
+Route::get('/instructors', [instructorsController::class, 'instructors'])->name('instructors');
+
+// Halaman Enroll
 Route::get('/enroll', function () {
     return view('enroll');
-});
+})->name('enroll');
+
+// Halaman Courses
+use App\Http\Controllers\coursesController;
+Route::get('/courses', [coursesController::class, 'courses'])->name('courses');
