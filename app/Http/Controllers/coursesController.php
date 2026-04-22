af920fb44b1a6819b81c2f71353b108412fe8e5f
NevCloud <fahadarifin15@gmail.com>
2026-04-22 22:51:31 +0700

    fix: list course

--- app/Http/Controllers/coursesController.php
+++ app/Http/Controllers/coursesController.php
@@ -8,40 +8,10 @@ use Illuminate\Support\Collection;
 
 class coursesController extends Controller
 {
-    public function courses(Request $request)
+    public function index()
     {
-        $courses = [
-            [
-                'title' => 'Desain Grafis',
-                'category' => 'DESIGN',
-                'price' => 49,
-                'image' => 'img/momo.png',
-                'author' => 'Fahad Arifin',
-                'role' => 'Graphic Designer',
-                'rating' => 4.9,
-                'students' => 79,
-            ],
-            [
-                'title' => 'Fullstack Web Dev',
-                'category' => 'CODE',
-                'price' => 89,
-                'image' => 'https://images.unsplash.com/photo-1498050108023-c5249f4df085',
-                'author' => 'Budi Santoso',
-                'role' => 'Fullstack Engineer',
-                'rating' => 5.0,
-                'students' => 159,
-            ],
-            [
-                'title' => 'Data Science',
-                'category' => 'DATA',
-                'price' => 75,
-                'image' => 'https://images.unsplash.com/photo-1460925895917-afdab827c52f',
-                'author' => 'Siti Aminah',
-                'role' => 'Data Scientist',
-                'rating' => 4.8,
-                'students' => 49,
-            ],
-        ];
+        $courses = config('courses');
+
         return view('courses', compact('courses'));
     }
 }
---
+++ config/courses.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+    [
+        'title' => 'Desain Grafis',
+        'category' => 'DESIGN',
+        'price' => 49,
+        'image' => 'img/momo.png',
+        'author' => 'Fahad Arifin',
+        'role' => 'Graphic Designer',
+        'rating' => 4.9,
+        'students' => 79,
+    ],
+    [
+        'title' => 'Fullstack Web Dev',
+        'category' => 'CODE',
+        'price' => 89,
+        'image' => 'https://images.unsplash.com/photo-1498050108023-c5249f4df085',
+        'author' => 'Budi Santoso',
+        'role' => 'Fullstack Engineer',
+        'rating' => 5.0,
+        'students' => 159,
+    ],
+    [
+        'title' => 'Data Science',
+        'category' => 'DATA',
+        'price' => 75,
+        'image' => 'https://images.unsplash.com/photo-1460925895917-afdab827c52f',
+        'author' => 'Siti Aminah',
+        'role' => 'Data Scientist',
+        'rating' => 4.8,
+        'students' => 49,
+    ],
+];
